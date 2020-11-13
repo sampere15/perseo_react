6c004820a24ccf2d9e47089f41f51bac20933aaa
Adrián Sampere (Xiaomi Air) <sampere.adrian@gmail.com>
2020-11-13 17:55:15 +0100

    Adding react-router-dom and creating pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import Player from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/player" component={Player} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
